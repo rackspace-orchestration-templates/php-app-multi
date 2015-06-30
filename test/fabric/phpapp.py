3dae2dae1b4f0fd763bfaa6ade1bacc838b35ca1
Justin Phelps <linuturk@onitato.com>
2015-06-30 18:30:38 -0500

    removing tests for mysql

--- test/fabric/phpapp.py
+++ test/fabric/phpapp.py
@@ -14,18 +14,14 @@ def check():
         '/var/www/vhosts/application/index.php did not exist'
 
     assert port.is_listening(80), 'port 80/apache2 is not listening'
-    assert port.is_listening(3306), 'port 3306/mysqld is not listening'
     assert port.is_listening(11211), 'port 11211/memcached is not listening'
 
-    assert user.exists('mysql'), 'mysql user does not exist'
     assert user.exists('memcache'), 'memcache user does not exist'
 
     assert process.is_up('apache2'), 'apache2 is not running'
-    assert process.is_up('mysqld'), 'mysqld is not running'
     assert process.is_up('memcached'), 'memcached is not running'
 
     assert service.is_enabled('apache2'), 'apache2 service not enabled'
-    assert service.is_enabled('mysql'), 'mysql service not enabled'
     assert service.is_enabled('memcached'), 'memcached service not enabled'
 
     assert http_check(site, string), 'Apache is not responding as expected.'
